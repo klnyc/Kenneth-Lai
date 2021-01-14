22b2424de03d055d0cf29ad7ed7c8a7cbabae3b3
Kenneth Lai <klnyc6@gmail.com>
2021-01-14 17:15:55 -0500

    removed react

--- client/components/Home.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Home = () => {
-    return (
-        <div className="home">Kenneth Lai</div>
-    )
-}
-
-export default Home
--- client/components/Index.js
+++
@@ -1,7 +0,0 @@
-import Navigation from './Navigation'
-import Home from './Home'
-import Projects from './Projects'
-import Photography from './Photography'
-import Biography from './Biography'
-
-export { Navigation, Home, Projects, Photography, Biography }
