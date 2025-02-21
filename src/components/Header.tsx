9ef2b9897021984aca28256324479c5797149caa
Kenneth Lai <kennethlai.ny@gmail.com>
2025-02-20 21:54:38 -0500

    refactor header links into React components

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,49 +5,54 @@ interface HeaderProps {
   setSelectedSection: (section: SectionType) => void;
 }
 
-export const Header = ({
+interface SectionLinkProps extends HeaderProps {
+  section: SectionType;
+}
+
+const PersonalLinks = (): JSX.Element => {
+  return (
+    <div className="personal_links">
+      <a href="https://www.linkedin.com/in/kennethklai/">
+        <img src="icons/linkedin.svg" />
+      </a>
+      <a href="https://github.com/klnyc">
+        <img src="icons/github.svg" />
+      </a>
+      <a href="https://www.instagram.com/kl.nyc/">
+        <img src="icons/instagram.svg" />
+      </a>
+      <a href="https://www.youtube.com/watch?v=49xpx7a_prY">
+        <img src="icons/youtube.svg" />
+      </a>
+    </div>
+  );
+};
+
+const SectionLink = ({
+  section,
   selectedSection,
   setSelectedSection,
-}: HeaderProps): JSX.Element => {
-  const renderSectionLink = (section: SectionType): JSX.Element => {
-    return (
-      <span
-        className={`px-2 ${
-          section === selectedSection ? "section_link_selected" : ""
-        }`}
-        onClick={() => setSelectedSection(section)}
-      >
-        {section}
-      </span>
-    );
-  };
+}: SectionLinkProps): JSX.Element => {
+  return (
+    <span
+      className={`px-2 ${
+        section === selectedSection ? "section_link_selected" : ""
+      }`}
+      onClick={() => setSelectedSection(section)}
+    >
+      {section}
+    </span>
+  );
+};
 
-  const renderSectionLinks = (): JSX.Element => {
+export const Header = (props: HeaderProps): JSX.Element => {
+  const SectionLinks = (): JSX.Element => {
     return (
       <div className="section_links">
         <div className="title_initials">KL</div>
-        {Object.values(SectionType).map((section) =>
-          renderSectionLink(section)
-        )}
-      </div>
-    );
-  };
-
-  const renderPersonalLinks = (): JSX.Element => {
-    return (
-      <div className="personal_links">
-        <a href="https://www.linkedin.com/in/kennethklai/">
-          <img src="icons/linkedin.svg" />
-        </a>
-        <a href="https://github.com/klnyc">
-          <img src="icons/github.svg" />
-        </a>
-        <a href="https://www.instagram.com/kl.nyc/">
-          <img src="icons/instagram.svg" />
-        </a>
-        <a href="https://www.youtube.com/watch?v=49xpx7a_prY">
-          <img src="icons/youtube.svg" />
-        </a>
+        {Object.values(SectionType).map((section) => (
+          <SectionLink section={section} {...props} />
+        ))}
       </div>
     );
   };
@@ -55,8 +60,8 @@ export const Header = ({
   return (
     <div className="header p-4 m-0">
       <div className="title">KENNETH LAI</div>
-      {renderSectionLinks()}
-      {renderPersonalLinks()}
+      <SectionLinks />
+      <PersonalLinks />
     </div>
   );
 };
