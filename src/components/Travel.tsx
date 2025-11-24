57efeb9e835df9e06354ca67f52d151d65fdaa07
Kenneth Lai <kennethlai.ny@gmail.com>
2025-11-24 15:52:52 -0500

    Add photo skeleton

--- src/components/Travel.tsx
+++ src/components/Travel.tsx
@@ -1,21 +1,32 @@
+import { useState } from "react";
 import { photos } from "../photos";
 
-export const Travel = (): JSX.Element => {
-  const renderPhoto = (fileName: string): JSX.Element => {
-    return (
-      <div key={fileName} className="photo">
-        <img src={`photos/${fileName}`} />
-        <div className="photo-overlay">
-          <div className="overlay-text">{fileName.split(".")[0]}</div>
-        </div>
-      </div>
-    );
-  };
+interface PhotoProps {
+  fileName: string;
+}
 
+const Photo = ({ fileName }: PhotoProps): JSX.Element => {
+  const [loading, setLoading] = useState(true);
+
+  return (
+    <div key={fileName} className="photo">
+      {loading && <div className="photo-skeleton"></div>}
+
+      <img src={`photos/${fileName}`} onLoad={() => setLoading(false)} />
+      <div className="photo-overlay">
+        <div className="overlay-text">{fileName.split(".")[0]}</div>
+      </div>
+    </div>
+  );
+};
+
+export const Travel = (): JSX.Element => {
   return (
     <div id="travel" className="section">
       <div id="photo-grid">
-        {photos.map((fileName) => renderPhoto(fileName))}
+        {photos.map((fileName) => (
+          <Photo fileName={fileName} />
+        ))}
       </div>
     </div>
   );
