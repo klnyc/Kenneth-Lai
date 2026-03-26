fa7febfcdfc66fe345dfefaec17f721fd9f14b15
Kenneth Lai <kennethlai.ny@gmail.com>
2026-03-26 09:57:43 -0400

    Add years of experience to About section

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -2,12 +2,14 @@ export const About = (): JSX.Element => {
   return (
     <div id="about" className="section">
       <div>
-        Driven creator with a passion for designing, building, and scaling
-        products to their full potential.
+        Frontend software engineer with experience working at Amazon (AWS),
+        Fareportal and Parsons Corporation, totaling 12+ years of professional
+        experience.
       </div>
       <div>
-        Inspired by technology who commits to precision and accuracy, not just
-        in engineering, but in life as well.
+        Driven creator inspired by technology with a passion for designing,
+        building, and scaling products to their full potential. Committed to
+        precision and accuracy, not just in engineering, but in life as well.
       </div>
     </div>
   );
